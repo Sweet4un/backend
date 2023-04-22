3cc95b8bb2f1a4c16bf0981e01bb17d02f1e545d
Sweet4un <131378665+Sweet4un@users.noreply.github.com>
2023-04-22 12:01:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileUpload");
+const cors = require("cors")
 
+app.use(cors)
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
