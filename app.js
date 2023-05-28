db2586f5d632d666932e8506a6d32996ffb9be96
Sweet4un <131378665+Sweet4un@users.noreply.github.com>
2023-05-28 23:37:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ const cors = require("cors")
 // });
 
 
-app.use(cors('*'));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
